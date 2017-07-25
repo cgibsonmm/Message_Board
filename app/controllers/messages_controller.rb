0162ac8ce3c6ade44dbabc8eb9719f041c05547e
Corey Gibson <cgibsonmm@gmail.com>
2017-07-25 16:46:04 -0400

    error fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -46,7 +46,7 @@ class MessagesController < ApplicationController
   private
 
     def message_params
-      params.require(:message).permit(:title, :description)
+      params.require(:message).permit(:title, :description, :user_id)
     end
 
     def find_message
